8b4cb62f0b595223a39584c689f911695d8c987d
hackerrank <me@hackerrank.com>
2020-01-26 20:33:30 -0500

    registration form

--- GeekTextProject/BookBrowseSortApp/views.py
+++ GeekTextProject/BookBrowseSortApp/views.py
@@ -1,12 +1,13 @@
 """
 Mode Log
 1/25 Miguel defined Http request for home and books URLs
-1/26 Omar defined login 
+1/26 Omar defined login and register
 """
 
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Book
+from .forms import RegistrationForm
 
 def home(request):
      return render(request, 'home.html')
@@ -18,4 +19,16 @@ def books(request):
 
 def login(request):
      return render(request, 'login.html')
+
+def register(request):
+     if request.method == 'POST':
+          form = RegistrationForm(request.POST)
+          if form.is_valid():
+               form.save()
+               return redirect('/login')
+     else:
+          form= RegistrationForm()
+
+          args = {'form': form}
+          return render(request, 'register.html', args)
 
--- GeekTextProject/GeekText/urls.py
+++ GeekTextProject/GeekText/urls.py
@@ -2,7 +2,7 @@
 1/25 Miguel imported url
      defined url for home and books
      added entry for static files for the project
-1/26 Omar created login url
+1/26 Omar created login url and registration
 """
 from django.contrib import admin
 from django.urls import path
@@ -10,11 +10,13 @@ from BookBrowseSortApp import views
 from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
     url(r'^books/', views.books, name='books'),
-    url(r'^login/$', LoginView.as_view(template_name='login.html'), name='login')
+    url(r'^login/$', LoginView.as_view(template_name='login.html'), name='login'),
+    url(r'^register/$', views.register, name ='register')
 ]+ static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
